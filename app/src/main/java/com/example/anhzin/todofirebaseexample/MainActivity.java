e037d1730889ab7f80213fb72bc2d1da308e0604
nguyenanhzin <dao.nguyen@smoovapp.com>
2016-08-16 14:42:29 +0700

    first commit

---
+++ app/src/main/java/com/example/anhzin/todofirebaseexample/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.anhzin.todofirebaseexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get ListView object from xml
+        final ListView listView = (ListView) findViewById(R.id.listView);
+
+        // Create a new Adapter
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1);
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // Use Firebase to populate the list.
+        Firebase.setAndroidContext(this);
+
+        new Firebase("https://todo-9927a.firebaseio.com/todoItems")
+                .addChildEventListener(new ChildEventListener() {
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        adapter.add((String)dataSnapshot.child("text").getValue());
+                    }
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+                        adapter.remove((String)dataSnapshot.child("text").getValue());
+                    }
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) { }
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) { }
+                    public void onCancelled(FirebaseError firebaseError) { }
+                });
+
+        // Add items via the Button and EditText at the bottom of the window.
+        final EditText text = (EditText) findViewById(R.id.todoText);
+        final Button button = (Button) findViewById(R.id.addButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new Firebase("https://todo-9927a.firebaseio.com/todoItems")
+                        .push()
+                        .child("text")
+                        .setValue(text.getText().toString());
+            }
+        });
+        // Delete items when clicked
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                new Firebase("https://todo-9927a.firebaseio.com/todoItems")
+                        .orderByChild("text")
+                        .equalTo((String) listView.getItemAtPosition(position))
+                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                if (dataSnapshot.hasChildren()) {
+                                    DataSnapshot firstChild = dataSnapshot.getChildren().iterator().next();
+                                    firstChild.getRef().removeValue();
+                                }
+                            }
+                            public void onCancelled(FirebaseError firebaseError) { }
+                        });
+            }
+        });
+    }
+}
